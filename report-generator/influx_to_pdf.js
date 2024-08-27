b27d0944c6d9bed746b965ea0073f8a5ff089245
Putu Oki Wiradita Aryawan <44687773+oki-aryawan@users.noreply.github.com>
2024-08-27 13:30:16 +0700

    Update influx_to_pdf.js

--- report-generator/influx_to_pdf.js
+++ report-generator/influx_to_pdf.js
@@ -1,43 +1,37 @@
 let influxData = msg.payload;
-let memoryAvailable = {};
-let memoryCache = {};
-let memoryUsed = {};
+let temperatureData = {};
+let humidityData = {};
 
 influxData.forEach(entry => {
     let time = new Date(entry._time).toLocaleTimeString();
-    if (entry._measurement === "Memory Available") {
-        memoryAvailable[time] = entry._value;
-    } else if (entry._measurement === "Memory Chace") {
-        memoryCache[time] = entry._value;
-    } else if (entry._measurement === "Memory Used") {
-        memoryUsed[time] = entry._value;}
+    if (entry._measurement === "temp") {
+        temperatureData[time] = entry._value;
+    } else if (entry._measurement === "hum") {
+        humidityData[time] = entry._value;
+    }
 });
 
 let data = [];
 
-Object.keys(memoryUsed).forEach(time => {
-    let usedMB = memoryUsed[time];
-    let cacheMB = memoryCache[time] || '';
-    let availableMB = memoryAvailable[time];
-    let usedPercentage = (usedMB / (usedMB + availableMB)) * 100;
+Object.keys(temperatureData).forEach(time => {
+    let tempC = temperatureData[time];
+    let humPercentage = humidityData[time] || ''; // Humidity might not be available for some times
 
     data.push([{ text: time, alignment: 'center' },
-    { text: usedPercentage.toFixed(2), alignment: 'center' },
-    { text: usedMB.toFixed(2), alignment: 'center' },
-    { text: cacheMB.toFixed(2), alignment: 'center' }]);
+    { text: tempC.toFixed(2), alignment: 'center' },
+    { text: humPercentage ? humPercentage.toFixed(2) : '', alignment: 'center' }]);
 });
 
 // Format PDF content
 msg.payload = {
     content: [
-        { text: 'Memory Report', style: 'header' },
+        { text: 'Environmental Report', style: 'header' },
         {
             table: {
                 body: [
                     [{ text: 'Time', alignment: 'center' },
-                    { text: 'Memory Used (MB)', alignment: 'center' },
-                    { text: 'Memory Used (%)', alignment: 'center' },
-                    { text: 'Memory Cache (MB)', alignment: 'center' }],
+                    { text: 'Temperature (C)', alignment: 'center' },
+                    { text: 'Humidity (%)', alignment: 'center' }],
                     ...data
                 ]
             }
