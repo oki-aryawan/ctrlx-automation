963c672fd83a6f7064eee35ce97d076192e30d5e
Putu Oki Wiradita Aryawan <44687773+oki-aryawan@users.noreply.github.com>
2024-10-15 13:08:52 +0700

    Update main_v3.pas

--- greenhouse/main_v3.pas
+++ greenhouse/main_v3.pas
@@ -3,21 +3,15 @@ VAR
 	status: BOOL; str_status: STRING;
 	start: BOOL; stop: BOOL;
 	str_operation: STRING;
-	temp: DINT := 0; 
-	heater: BOOL := FALSE;
-	cool: BOOL := FALSE;
-	humidifier: BOOL := FALSE;
-	dehumidifier: BOOL := FALSE;
-	heater_status: STRING := 'OFF';
-	cool_status: STRING := 'OFF';
-	temp_thres: DINT:= 30;
+	temp: DINT := 0; temp_thres: DINT:= 30;
+	heater: BOOL := FALSE; heater_status: STRING := 'OFF';
+	cool: BOOL := FALSE; cool_status: STRING := 'OFF';
 	random: DINT;
     seed: DINT := 12345;
     a: DINT := 22695477;
     c: DINT := 1;
     m: DINT := 2147483647;
-    timer: TON;
-    timerInterval: TIME := T#2S;
+    timer: TON; timerInterval: TIME := T#2S;
 END_VAR
 
 timer(IN := TRUE, PT:= timerInterval); 
@@ -28,21 +22,14 @@ IF status THEN
 		seed := (a * seed + c) MOD m;
 		temp := 25 + (seed MOD 21);
 	END_IF;
-	IF start THEN
-		str_operation := 'ON';
-		IF temp > temp_thres THEN
-			cool := TRUE; cool_status := 'ON';
-			heater := FALSE; heater_status := 'OFF';
-		ELSE
-			cool := FALSE; cool_status := 'OFF';
-			heater := TRUE; heater_status := 'ON';
-		END_IF;
-	ELSE
-		str_operation := 'OFF';
-	END_IF;
-	IF stop THEN
-		start := FALSE;
-	END_IF;
+    IF temp > temp_thres THEN
+        cool := TRUE; cool_status := 'ON';
+        heater := FALSE; heater_status := 'OFF';
+    ELSE
+        cool := FALSE; cool_status := 'OFF';
+        heater := TRUE; heater_status := 'ON';     
+    END_IF;
 ELSE
-	start := FALSE; stop := FALSE; str_status := 'OFF';
+	str_status := 'OFF'; cool := FALSE; heater := FALSE;
+	heater_status := 'OFF'; cool_status := 'OFF';
 END_IF;
